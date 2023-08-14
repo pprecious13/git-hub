6057297ec4c3effa034611064bb41ac794970a7b
precious <precious>
2023-08-14 10:02:41 -0400

    Initial Commit

---
+++ git hub/git_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_hubApp.swift
+//  git hub
+//
+//  Created by Hana Akoto Baffour on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct git_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
